89327de30f931f33523a122c6ad17cdaaa26601a
jeon won uk <vpzj1234@gmail.com>
2017-06-13 19:18:45 +0900

    rr

--- src/main/java/com/parker/user/controller/UserController.java
+++ src/main/java/com/parker/user/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 
 	@Autowired
 	BCryptPasswordEncoder passwordEncooder;
-	
+
 	@Autowired
 	UserService userService;
 
@@ -68,12 +68,6 @@ public class UserController {
 		return "/user/useridcheckresult";
 	}
 
-	// 로그아웃
-	/*
-	 * @RequestMapping(value ="/userlogin") public String logout(HttpSession
-	 * session) { session.setAttribute("UVO", null); return "user/userlogin"; }
-	 */
-
 	// 로그인폼
 	@RequestMapping(value = "/userlogin", method = RequestMethod.GET)
 	public String userlogin(HttpSession session, @ModelAttribute UserVO UVO) {
@@ -82,57 +76,37 @@ public class UserController {
 	}
 
 	// 로그인
-	/*
-	 * @RequestMapping(value = "/login", method = RequestMethod.POST) public
-	 * String login(@ModelAttribute UserVO UVO, Model model, HttpSession
-	 * session, HttpServletRequest request) { logger.info("login 호출 성공"); String
-	 * url = ""; int result = 0;
-	 * 
-	 * String userid = request.getParameter("user_id"); String userpass =
-	 * request.getParameter("user_pass"); System.out.println("받아온 userid" +
-	 * userid); UVO = userService.selectList(UVO); System.out.println("UVO아이디" +
-	 * UVO.getUser_id()); logger.info("result=" + result);
-	 * 
-	 * if (UVO.getUser_id().equals(userid)) { session.setAttribute("UVO",
-	 * UVO.getUser_id()); model.addAttribute("UVO",UVO.getUser_id()); url =
-	 * "user/login"; } else { session.invalidate(); url = "user/login"; } return
-	 * url;
-	 * 
-	 * }
-	 */
-
-	// 로그인
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ModelAndView loginProcess(@ModelAttribute UserVO UVO, HttpSession session, HttpServletRequest request) {
 		logger.info("loginProcess 호출 성공");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("redirect:userlogin.do");
 
-		
 		String userid = request.getParameter("user_id");
 		String pass = request.getParameter("user_password");
-		/*String shapass = passwordEncooder.encode(pass);*/
-		
-		System.out.println("DB 비밀번호 UVO.getUser_password() : "+UVO.getUser_password());
-		
-		System.out.println("입력한 비밀번호 pass : "+pass);
-		
+		/* String shapass = passwordEncooder.encode(pass); */
+
+		System.out.println("DB 비밀번호 UVO.getUser_password() : " + UVO.getUser_password());
+
+		System.out.println("입력한 비밀번호 pass : " + pass);
+
 		System.out.println("UVO tostring :" + UVO.toString());
 		UserVO userLogin = userService.sessionLogin(UVO);
-		
-		System.out.println(" userLogin : "+userLogin);
-		
-		
-		/*System.out.println(" 컨트롤러 UVO.toString() : " +UVO.toString());
-		System.out.println(" 컨트롤러 UVO.toString() : " +userid);
-		System.out.println(" 컨트롤러 UVO.toString() : " +UVO.getUser_password());
-		System.out.println(" 컨트롤러 UVO.toString() : " +shapass);
-		*/
-		//비밀번호도 비교해야됨
+
+		System.out.println(" userLogin : " + userLogin);
+
+		/*
+		 * System.out.println(" 컨트롤러 UVO.toString() : " +UVO.toString());
+		 * System.out.println(" 컨트롤러 UVO.toString() : " +userid);
+		 * System.out.println(" 컨트롤러 UVO.toString() : "
+		 * +UVO.getUser_password());
+		 * System.out.println(" 컨트롤러 UVO.toString() : " +shapass);
+		 */
+		// 비밀번호도 비교해야됨
 		if (UVO.getUser_id().equals(userid) && UVO.getUser_password().equals(pass)) {
 			session.setAttribute("UVO", UVO);
 			System.out.println("성공");
-		}else if(!UVO.getUser_id().equals(userid)) {
+		} else if (!UVO.getUser_id().equals(userid)) {
 			System.out.println("실패");
 		}
 
@@ -147,7 +121,7 @@ public class UserController {
 		session.invalidate();
 		return "redirect:/";
 	}
-	
+
 	@RequestMapping(value = "/page1", method = RequestMethod.GET)
 	public String page(HttpSession session, @ModelAttribute UserVO UVO) {
 		logger.info("userpage1 호출 성공");
